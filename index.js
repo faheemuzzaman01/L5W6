6193710047b85fe71efa486b25cf1a7ea651fb66
faheemuzzaman01 <161707364+faheemuzzaman01@users.noreply.github.com>
2025-03-27 09:47:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -91,4 +91,4 @@ app.delete("/student/:id", async (req, res) => {
   }
 });
 
-// app.listen(3000, () => console.log("Server is running on port 3000"));
+app.listen(3000, () => console.log("Server is running on port 3000"));
